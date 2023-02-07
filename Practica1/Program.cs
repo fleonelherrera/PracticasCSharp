02f421ab9b367da777184619c4b0228a5466fa11
Franco <francoherrera77003@gmail.com>
2023-02-07 19:46:45 -0300

    Agregar archivos de proyecto.

---
+++ Practica1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escriba un programa de aplicación que solicite al usuario ingresar su nombre y apellido, e imprima por consola un saludo personalizado
+            // usando ese nombre e informe entre () la cantidad de letras que conforman la oración impresa. Ejemplo:
+            // Hola Juan Perez, bienvenido a la materia (40)
+
+            Console.WriteLine("Ingrese su nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su apellido: ");
+            string apellido = Console.ReadLine();
+
+            string saludo = "Hola " + nombre + " " + apellido + ", bienvenido a la materia";
+
+            int cantCaracteres = saludo.Length;
+
+            Console.WriteLine(saludo + " (" + cantCaracteres + ")");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practica2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Escriba un programa que calcule la suma de dos números reales introducidos por teclado y muestre el resultado en consola.
+            // Ayuda: utilice Double.Parse(st) para obtener el valor real del string st.
+
+            Console.WriteLine("Ingrese el primer número: ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo número: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double resultado = num1 + num2;
+
+            Console.WriteLine("El resultado de la suma es: " + resultado);
+
+            Console.ReadKey();
+        }
+    }
+}
